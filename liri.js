91d9a0ccc351b74c57126c2d5b986cd7605d4d5e
Matt Vanley <matt@vibrantsitedesign.com>
2019-04-27 13:02:43 -0500

    update

--- liri.js
+++ liri.js
@@ -1,29 +1,27 @@
-require("dotenv").config();
+//require(".env").config();
 
 const keys = require("./keys.js");
 
-const spotify = new Spotify(keys.spotify);
+//const spotify = new Spotify(keys.spotify);
 
 //concert-this ---COMMAND--- node liri.js concert-this <artist/band name here>
-"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+//"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
 
 //spotify-this-song ---COMMAND--- node liri.js spotify-this-song '<song name here>'
-const Spotify = require('node-spotify-api')
+//const Spotify = require('node-spotify-api')
 
 //movie-this ---COMMAND--- node liri.js movie-this '<movie name here>'
 const axios = require("axios");
-
-// Store all of the arguments in an array
 const nodeArgs = process.argv;
+let movieName = "";
+const command = process.argv[2];
 
-// Create an empty variable for holding the movie name
-const movieName = "";
 
-// Loop through all the words in the node argument
-// And do a little for-loop magic to handle the inclusion of "+"s
-for (var i = 2; i < nodeArgs.length; i++) {
 
-    if (i > 2 && i < nodeArgs.length) {
+
+for (var i = 3; i < nodeArgs.length; i++) {
+
+    if (i > 3 && i < nodeArgs.length) {
         movieName = movieName + "+" + nodeArgs[i];
     }
     else {
@@ -31,17 +29,21 @@ for (var i = 2; i < nodeArgs.length; i++) {
 
     }
 }
+if (command === "movie-this") {
+movie();
+}
 
-// Run a request with axios to the OMDB API with the movie specified
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=b38aee4";
+function movie(){
+    const queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+    console.log(queryUrl);
+    axios.get(queryUrl).then(
+        function (response) {
+            console.log("Release Year: " + response.data.Year);
+        }
+    
+    );
+}
 
-// This line is just to help us debug against the actual URL.
-console.log(queryUrl);
-
-axios.get(queryUrl).then(
-    function (response) {
-        console.log("Release Year: " + response.data.Year);
-    }
-);
 
 //do-what-it-says ---COMMAND--- node liri.js do-what-it-says
+

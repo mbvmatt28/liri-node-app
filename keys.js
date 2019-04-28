53abf46785ebf14fe260ccd4f4d6a467250c0e7c
Matthew Vanley <matt@vibrantsitedesign.com>
2019-04-28 18:23:46 -0500

    update

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.f9223a471ea64d389028520e2b57dc25,
-  secret: process.env.SPOTIFY_SECRET
+  id: process.env.f9223a471ea64d389028520e2b57dc25, 
+  secret: process.env.f3520621797b45ada5da5a3dd1119e0f
 };
--- liri.js
+++ liri.js
@@ -1,23 +1,92 @@
-//require(".env").config();
-
-const keys = require("./keys.js");
-
-//const spotify = new Spotify(keys.spotify);
-
-//concert-this ---COMMAND--- node liri.js concert-this <artist/band name here>
-//"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-
-//spotify-this-song ---COMMAND--- node liri.js spotify-this-song '<song name here>'
-//const Spotify = require('node-spotify-api')
-
-//movie-this ---COMMAND--- node liri.js movie-this '<movie name here>'
+//require("dotenv").config();
 const axios = require("axios");
 const nodeArgs = process.argv;
-let movieName = "";
 const command = process.argv[2];
 
 
 
+
+const keys = require("./keys.js");
+
+//const Spotify = new Spotify(keys.spotify);
+
+
+
+//concert-this ---COMMAND--- node liri.js concert-this <artist/band name here>
+let artist = "";
+
+for (var i = 3; i < nodeArgs.length; i++) {
+
+    if (i > 3 && i < nodeArgs.length) {
+        artist = artist + "+" + nodeArgs[i];
+    }
+    else {
+        artist += nodeArgs[i];
+
+    }
+}
+if (command === "concert-this") {
+    band();
+}
+function band() {
+    let queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    axios.get(queryUrl).then(
+        function (response){
+            console.log("Venue: " + response.data.venue.name);
+            console.log("Location: " + response.data.venue.city);
+            console.log("Date: " + response.data.datetime);
+            
+            
+        }
+    )
+}
+
+
+//spotify-this-song ---COMMAND--- node liri.js spotify-this-song '<song name here>'
+//let Spotify = require('node-spotify-api')
+
+let spot = "";
+
+for (var i = 3; i < nodeArgs.length; i++) {
+
+    if (i > 3 && i < nodeArgs.length) {
+        spot = spot + "+" + nodeArgs[i];
+    }
+    else {
+        spot += nodeArgs[i];
+
+    }
+}
+if (command === "spotify-this-song") {
+    spotify();
+}
+function spotify() {
+    let queryUrl = "https://api.spotify.com/v1/" + spot;
+    axios.get(queryUrl).then(
+        function (response){
+            console.log(response.data)
+            
+            
+        }
+    )
+}
+//var Spotify = require('node-spotify-api');
+ 
+//let Spotify = new Spotify({
+ // id: f9223a471ea64d389028520e2b57dc25,
+ // secret: f3520621797b45ada5da5a3dd1119e0f
+//});
+ 
+//spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+//  if (err) {
+//    return console.log('Error occurred: ' + err);
+ // }
+ 
+//console.log(data); 
+//});
+//movie-this ---COMMAND--- node liri.js movie-this '<movie name here>'
+
+let movieName = "";
 
 for (var i = 3; i < nodeArgs.length; i++) {
 
@@ -30,20 +99,28 @@ for (var i = 3; i < nodeArgs.length; i++) {
     }
 }
 if (command === "movie-this") {
-movie();
+    movie();
 }
 
-function movie(){
-    const queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+function movie() {
+    let queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
     console.log(queryUrl);
     axios.get(queryUrl).then(
         function (response) {
+            console.log("Title: " + response.data.Title);
             console.log("Release Year: " + response.data.Year);
+            console.log("OMDB Rating: " + response.data.imdbRating);
+            console.log("Country: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+            
+            
         }
-    
+
     );
 }
 
 
-//do-what-it-says ---COMMAND--- node liri.js do-what-it-says
+
 
